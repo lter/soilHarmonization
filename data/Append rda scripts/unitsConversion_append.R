71966947890b5603a0e5523b0a786700eb2f1b9a
Unknown <drk.pierson@gmail.com>
2020-09-07 11:45:29 -0600

    Bug fix for unitsConversion change

--- data/Append rda scripts/unitsConversion_append.R
+++ data/Append rda scripts/unitsConversion_append.R
@@ -4,15 +4,18 @@ library(tidyverse)
 #########################
 setwd('C:/github/RCsoilHarmonization/data')
 
-load("unitsCOnversions.rda")
+load("unitsConversions.rda")
 
 new_units <- unitsConversions %>% filter(var == "n_min") %>% 
                                   mutate(var = "n_min_pot") %>%
                                   mutate(Var_long = "Net N mineralization at water holding capacity")
 
-unitsConversions_updated <- unitsConversions %>% add_row(new_units)
+unitsConversions <- unitsConversions %>% add_row(new_units)
 
-save(unitsConversions_updated, file = "unitsCOnversions.rda")
+#To fix mistakes, cut off rows (3 row cut shown here)
+#unitsConversions = slice(unitsConversions, 1:(n()-3))
+
+save(unitsConversions, file = "unitsConversions.rda")
 
 
 # Check if changes are saved
